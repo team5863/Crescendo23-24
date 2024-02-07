2064c27d7305ac9445d9216e4a17eeafe9d20574
Team Hypercubed <5863.Hypercubed@gmail.com>
2024-02-06 18:33:49 -0600

    Update ShooterSub.java

--- src/main/java/frc/robot/subsystems/ShooterSub.java
+++ src/main/java/frc/robot/subsystems/ShooterSub.java
@@ -5,10 +5,27 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class ShooterSub extends SubsystemBase {
+
+  public static CANSparkMax topNEOMotor;
+  public static CANSparkMax bottomNEOMotor;
+
   /** Creates a new ShooterSub. */
-  public ShooterSub() {}
+  public ShooterSub() {
+    topNEOMotor = new CANSparkMax(Constants.Drivetrain.topNEOMotoriD, MotorType.kBrushless);
+    bottomNEOMotor = new CANSparkMax(Constants.Drivetrain.bottomNEOMotoriD, MotorType.kBrushless);
+    
+  }
+
+  public void set(double speed){
+    topNEOMotor.set(speed);
+    bottomNEOMotor.set(speed);
+  }
 
   @Override
   public void periodic() {
